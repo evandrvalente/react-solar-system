75aa06cae5ff189faca7a100c4871eb16fa2e293
Evandro Valente <evandr.valente@gmail.com>
2022-02-18 18:45:33 -0300

    Added Solar System Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import Header from './Header';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
     return (
-      <Header />
+      <>
+        <Header />
+        <SolarSystem />
+      </>
     );
   }
 }
