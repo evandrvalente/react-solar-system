e1d29385bd0944665e07781cdea7c11b3f840a55
Evandro Valente <evandr.valente@gmail.com>
2022-02-19 14:54:56 -0300

    feat: Added Missions Component

---
+++ src/components/Missions.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Missions extends React.Component {
+  render() {
+    return (
+      <div data-testid="missions">
+        roi
+      </div>
+    );
+  }
+}
+
+export default Missions;
